1455c5b4c462821cc35d86cedbee5c005df5615d
George <gex7777@gmail.com>
2022-10-22 20:31:41 +0530

    fix alignment containfo

--- components/ContactInfo.jsx
+++ components/ContactInfo.jsx
@@ -11,56 +11,58 @@ const ContactInfo = () => {
           <h1 className="text-5xl font-bold">Lets talk</h1>
           <p className="py-6">We are availabe on facebook and whatsapp!</p>
         </div>
-        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <div className="card-body text-center">
-            <div className="self-center">
-              <BiMessageDots size={100} />
+        <div className="flex flex-col justify-between gap-4">
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body text-center">
+              <div className="self-center">
+                <BiMessageDots size={100} />
+              </div>
+              <div className="">message us on</div>
+              <div className="flex justify-center">
+                <span>
+                  <a
+                    target="_blank"
+                    href=" https://wa.me/+919778398197"
+                    className="cursor-pointer"
+                  >
+                    <AiOutlineWhatsApp size={25} />
+                  </a>
+                </span>
+                <span>
+                  <a
+                    target="_blank"
+                    href=" https://m.me/109946811906765"
+                    className="cursor-pointer"
+                  >
+                    <RiMessengerLine size={25} />
+                  </a>
+                </span>
+              </div>
+              <div className="">jancyteachersfoodproducts@gmail.com</div>
             </div>
-            <div className="">message us on</div>
-            <div className="flex justify-center">
-              <span>
-                <a
-                  target="_blank"
-                  href=" https://wa.me/+919778398197"
-                  className="cursor-pointer"
-                >
-                  <AiOutlineWhatsApp size={25} />
-                </a>
-              </span>
-              <span>
-                <a
-                  target="_blank"
-                  href=" https://m.me/109946811906765"
-                  className="cursor-pointer"
-                >
-                  <RiMessengerLine size={25} />
-                </a>
-              </span>
-            </div>
-            <div className="">jancyteachersfoodproducts@gmail.com</div>
-          </div>
-        </div>{" "}
-        <div className="card  flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <div className="card-body text-center">
-            <div className="self-center">
-              <IoCallOutline size={100} />
-            </div>
-            <div className="">call us on </div>
-            <div className="flex justify-center">
-              <span>
-                <a
-                  target="_blank"
-                  href=" https://wa.me/+919778398197"
-                  className="cursor-pointer"
-                >
-                  <AiOutlineWhatsApp size={25} />
-                </a>
-              </span>
-              <span>
-                <a target="_blank" href=" tel:+919778398197">
-                  +919778398197
-                </a>
-              </span>
+          </div>{" "}
+          <div className="card  flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body text-center">
+              <div className="self-center">
+                <IoCallOutline size={100} />
+              </div>
+              <div className="">call us on </div>
+              <div className="flex justify-center">
+                <span>
+                  <a
+                    target="_blank"
+                    href=" https://wa.me/+919778398197"
+                    className="cursor-pointer"
+                  >
+                    <AiOutlineWhatsApp size={25} />
+                  </a>
+                </span>
+                <span>
+                  <a target="_blank" href=" tel:+919778398197">
+                    +919778398197
+                  </a>
+                </span>
+              </div>
             </div>
           </div>
         </div>
