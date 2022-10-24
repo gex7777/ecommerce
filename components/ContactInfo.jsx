f7667be446896acf533b8dd2a397cafb97fd000e
George <gex7777@gmail.com>
2022-10-24 21:35:31 +0530

    fix responsivness

--- components/ContactInfo.jsx
+++ components/ContactInfo.jsx
@@ -6,13 +6,13 @@ import { RiMessengerLine } from "react-icons/ri";
 const ContactInfo = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row-reverse">
+      <div className="hero-content  flex-col lg:flex-row">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Lets talk</h1>
           <p className="py-6">We are availabe on facebook and whatsapp!</p>
         </div>
         <div className="flex flex-col justify-between items-center gap-4">
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card flex-shrink-0 w-full max-w-xs shadow-2xl bg-base-100">
             <div className="card-body text-center">
               <div className="self-center">
                 <BiMessageDots size={100} />
@@ -41,7 +41,7 @@ const ContactInfo = () => {
               <div className="">jancyteachersfoodproducts@gmail.com</div>
             </div>
           </div>{" "}
-          <div className="card  flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card  flex-shrink-0 w-full max-w-xs shadow-2xl bg-base-100">
             <div className="card-body text-center">
               <div className="self-center">
                 <IoCallOutline size={100} />
