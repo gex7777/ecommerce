01856b035f5b0e0fdda207f7c6f37791dd2fc135
George <gex7777@gmail.com>
2022-10-29 00:01:12 +0530

    re design product cards

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -10,17 +10,22 @@ const ProductList = ({ products }) => {
         {products &&
           products.map((product) => (
             <Link href={`/product/${product.slug.current}`} key={product._id}>
-              <div className="max-w-xs  card card-compact w-96 bg-base-100 shadow-xl">
-                <figure>
-                  <img
-                    src={urlFor(product.image && product.image[0])}
-                    alt={product.imageAlt}
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">{product.name}</h2>
-                  <p>starting at ₹{product.price}</p>
-                  <div className="card-actions justify-end">
+              <div
+                className="relative text-gray-50  hover:scale-105 transition-all ease-linear active:scale-95
+                w-80 h-96 rounded-2xl bg-cover"
+                style={{
+                  backgroundImage: `url(${urlFor(
+                    product.image && product.image[0]
+                  )})`,
+                }}
+              >
+                <div className="h-full w-full absolute bg-gradient-to-t from-slate-900 to-transparent rounded-2xl"></div>
+                <div className="absolute bottom-0 left-0  px-4 pb-4">
+                  <h2 className="text-2xl ">{product.name}</h2>
+                  <p className="text-xs py-2">
+                    starting at <div className="text-xl">₹{product.price}</div>
+                  </p>
+                  <div className="">
                     <button className="btn btn-primary">Order now</button>
                   </div>
                 </div>
