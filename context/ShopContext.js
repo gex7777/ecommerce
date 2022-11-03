4c39b4814a10a641007ff9e03d5fc438cd1adad5
George <gex7777@gmail.com>
2022-11-03 22:10:37 +0530

    update localstorage

--- context/ShopContext.js
+++ context/ShopContext.js
@@ -5,9 +5,9 @@ import useLocalStorage from "./../hooks/useLocalStorage";
 const ShoppingCartContext = createContext();
 
 export function ShoppingCartProvider({ children }) {
-  const [cart, setCart] = useLocalStorage("cart", []);
-  const [cartState, dispatch] = useReducer(shopReducer({ state: cart }), {
-    cart: cart,
+  const cart = JSON.parse(localStorage.getItem("cart"));
+  const [cartState, dispatch] = useReducer(shopReducer, {
+    cart: cart | [],
   });
   const [showCart, setShowCart] = useState(false);
   const totalQuantity = cartState.cart?.reduce(
