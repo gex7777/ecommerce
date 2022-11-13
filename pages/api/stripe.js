1ff708d62864d3f71ee7cc65197660c0246e5b51
George <gex7777@gmail.com>
2022-11-14 02:00:45 +0530

    add stripe payment

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -6,25 +6,46 @@ export default async function handler(req, res) {
     try {
       const params = {
         submit_type: "pay",
-        mode: "payment",
+        shipping_address_collection: {
+          allowed_countries: ["IN"],
+        },
         payment_method_types: ["card"],
         billing_address_collection: "auto",
         shipping_options: [{ shipping_rate: "shr_1M3kdxSIxv0ADR6m3ojocQqJ" }],
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: "{{PRICE_ID}}",
-            quantity: 1,
-          },
-        ],
+        line_items: req.body.map((item) => {
+          const img = item.image.asset._ref;
+          const newImage = img
+            .replace(
+              "image-",
+              `https://cdn.sanity.io/images/5jkxl6n1/production/`
+            )
+            .replace("-jpg", ".jpg");
+
+          return {
+            price_data: {
+              currency: "inr",
+              product_data: {
+                name: item.name + "(" + item.size + ")",
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+            },
+            quantity: item.quantity,
+          };
+        }),
         mode: "payment",
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
       };
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
-      res.redirect(303, session.url);
+      res.status(200).json(session);
     } catch (err) {
+      console.log(err.message);
       res.status(err.statusCode || 500).json(err.message);
     }
   } else {
