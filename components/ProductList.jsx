a6bef185db0db78e0efd3077d813951f6a5160f7
George <gex7777@gmail.com>
2022-11-23 23:08:18 +0530

    add filter heads

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -1,22 +1,26 @@
-import file from "@babel/core/lib/transformation/file/file";
-import Link from "next/link";
 import React, { useState } from "react";
-import { urlFor } from "../lib/client";
+
 import { ALL, CAKES, ACHAARS, SNACKS } from "./../constants";
+import FilterHeads from "./FilterHeads";
+import ProductCard from "./ProductCard";
 
 const filters = {
   [ALL]: () => true,
-  [CAKES]: (item) => item.type === CAKES,
-  [ACHAARS]: (item) => item.type === ACHAARS,
-  [SNACKS]: (item) => item.type === SNACKS,
+  [CAKES]: (item) => item.category === CAKES,
+  [ACHAARS]: (item) => item.category === ACHAARS,
+  [SNACKS]: (item) => item.category === SNACKS,
 };
 const ProductList = ({ products }) => {
   console.log(products);
   const [filter, setFilter] = useState(ALL);
 
   return (
-    <div className="p-4">
+    <div className="p-4 flex flex-col justify-center items-center">
       <h1 className="text-3xl font-bold mb-12">Products</h1>
+      <div className="py-4">
+        {" "}
+        <FilterHeads filter={filter} setFilter={setFilter} />
+      </div>
       <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 justify-items-center">
         {products &&
           products
