1cc78f433059dac1995354488dfcbf1c501c30e9
George <gex7777@gmail.com>
2022-11-23 22:16:47 +0530

    add filter defs

--- constants.js
+++ constants.js
@@ -5,3 +5,7 @@ export const BUSINESS_REGISTRATION_NUMBER = "";
 export const BUSINESS_ADDRESS =
   "   Eldho Kurakose building, Karayamparambu , karakutty PO, Kochi, KL,683576, India";
 export const BUSINESS_EMAIL = "jancyteachersfoodproducts@gmail.com";
+export const CAKES = "CAKES";
+export const ACHAARS = "ACHAARS";
+export const SNACKS = "SNACKS";
+export const ALL = "ALL";
