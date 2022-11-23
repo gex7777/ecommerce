da116b6a7d63a6610a14cea49496faa1c1a487ae
George <gex7777@gmail.com>
2022-11-23 22:17:07 +0530

    create filters

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -1,16 +1,27 @@
+import file from "@babel/core/lib/transformation/file/file";
 import Link from "next/link";
 import React, { useState } from "react";
 import { urlFor } from "../lib/client";
+import { ALL, CAKES, ACHAARS, SNACKS } from "./../constants";
+
+const filters = {
+  [ALL]: () => true,
+  [CAKES]: (item) => item.type === CAKES,
+  [ACHAARS]: (item) => item.type === ACHAARS,
+  [SNACKS]: (item) => item.type === SNACKS,
+};
 const ProductList = ({ products }) => {
   console.log(products);
-  const [filter, setFilter] = useState("");
+  const [filter, setFilter] = useState(ALL);
 
   return (
     <div className="p-4">
       <h1 className="text-3xl font-bold mb-12">Products</h1>
       <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 justify-items-center">
         {products &&
-          products.map((product) => <ProductCard product={product} />)}
+          products
+            .filter(filters[filter])
+            .map((product) => <ProductCard product={product} />)}
       </div>
     </div>
   );
