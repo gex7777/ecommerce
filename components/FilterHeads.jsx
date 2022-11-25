9c30f458e68982bea61f853232402396015d2e85
George <gex7777@gmail.com>
2022-11-25 20:30:34 +0530

    fix responsive overflow issue

--- components/FilterHeads.jsx
+++ components/FilterHeads.jsx
@@ -3,34 +3,44 @@ import { ALL, CAKES, SNACKS, ACHAARS, SPICES } from "../constants";
 
 const FilterHeads = ({ filter, setFilter }) => {
   return (
-    <div className="btn-group">
+    <div className="btn-group btn-group-horizontal">
       <button
-        className={`btn ${filter === ALL ? "btn-active" : ""}`}
+        className={`btn btn-xs sm:btn-sm lg:btn-lg ${
+          filter === ALL ? "btn-active" : ""
+        }`}
         onClick={() => setFilter(ALL)}
       >
         All
       </button>
       <button
-        className={`btn ${filter === CAKES ? "btn-active" : ""}`}
+        className={`btn btn-xs sm:btn-sm  lg:btn-lg ${
+          filter === CAKES ? "btn-active" : ""
+        }`}
         onClick={() => setFilter(CAKES)}
       >
         Cakes
       </button>
 
       <button
-        className={`btn ${filter === SNACKS ? "btn-active" : ""}`}
+        className={`btn btn-xs sm:btn-sm  lg:btn-lg ${
+          filter === SNACKS ? "btn-active" : ""
+        }`}
         onClick={() => setFilter(SNACKS)}
       >
         Snacks
       </button>
       <button
-        className={`btn ${filter === ACHAARS ? "btn-active" : ""}`}
+        className={`btn btn-xs sm:btn-sm  lg:btn-lg ${
+          filter === ACHAARS ? "btn-active" : ""
+        }`}
         onClick={() => setFilter(ACHAARS)}
       >
         Achaars
       </button>
       <button
-        className={`btn ${filter === SPICES ? "btn-active" : ""}`}
+        className={`btn btn-xs sm:btn-sm  lg:btn-lg ${
+          filter === SPICES ? "btn-active" : ""
+        }`}
         onClick={() => setFilter(SPICES)}
       >
         Spices
--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -19,7 +19,6 @@ const ProductList = ({ products }) => {
     <div className="p-4 flex flex-col justify-center items-center">
       <h1 className="text-3xl font-bold mb-12">Products</h1>
       <div className="py-4">
-        {" "}
         <FilterHeads filter={filter} setFilter={setFilter} />
       </div>
       <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 justify-items-center">
