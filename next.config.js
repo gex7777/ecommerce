311106f74240b9c35584a60dc1da74de2590eb01
George <gex7777@gmail.com>
2022-12-03 23:25:08 +0530

    add image optimisration form cdn

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    // assuming you were using the Sanity.io image CDN
+    // domains is an array of comma-separated strings
+    // ['cdn.sanity.io', 'cdn.not-sanity.io', 'another domain']
+    domains: ["cdn.sanity.io"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
