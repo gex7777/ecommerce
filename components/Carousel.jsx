63317f97697d81c4f0471905127cf49c371aef9a
George <gex7777@gmail.com>
2022-12-04 01:25:17 +0530

    make carosel image optimised

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -1,18 +1,22 @@
 import { Carousel as Caro } from "flowbite-react";
-import Image from "next/image";
+
+import Image from "next/future/image";
 import React from "react";
 import { urlFor } from "../lib/client";
 
 const Carousel = ({ images }) => {
   return (
     <div className="h-[80vh] bg-base-200">
-      <Caro>
+      <Caro className="relative">
         {images.map((image) => {
+          const src = urlFor(image).url();
           return (
             <Image
-              src={urlFor(image)}
+              src={src}
               alt="..."
               className="object-contain h-full lg:w-2/4"
+              width={400}
+              height={400}
             />
           );
         })}
