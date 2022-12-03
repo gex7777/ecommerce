b5c6c85988866f0dba6ac8cd051787753239c2b5
George <gex7777@gmail.com>
2022-12-03 23:40:46 +0530

    fix sanity image loading error

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { urlFor } from "../lib/client";
 import Link from "next/link";
+import Image from "next/image";
 const HeroBanner = ({ bannerData }) => {
   console.log(bannerData);
+  const src = urlFor(bannerData.image).url();
+
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <img
-          src={urlFor(bannerData.image)}
-          className="max-w-xs md:max-w-sm rounded-lg shadow-2xl"
-        />
+        {src && (
+          <Image
+            loader={() => src}
+            src={src}
+            width={340}
+            height={380}
+            className="max-w-xs md:max-w-sm rounded-lg shadow-2xl"
+          />
+        )}
         <div>
           <h1 className="text-5xl font-bold">{bannerData.title}</h1>
           <p className="py-6 ">{bannerData.desc}</p>
