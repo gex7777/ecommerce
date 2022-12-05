685229834910c61d3617cbf4e75f5e9bfd350b78
George <gex7777@gmail.com>
2022-12-05 12:34:58 +0530

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -8,26 +8,6 @@ import {
 } from "../components";
 import { client } from "../lib/client";
 
-const products = [
-  {
-    id: "1",
-    href: "/",
-    imageSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg",
-    imageAlt: "/",
-    name: "test",
-    price: "1000",
-  },
-  {
-    id: "2",
-    href: "/",
-    imageSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg",
-    imageAlt: "/",
-    name: "test",
-    price: "1000",
-  },
-];
 export default function Home({ products, bannerData }) {
   return (
     <>
@@ -36,7 +16,7 @@ export default function Home({ products, bannerData }) {
     </>
   );
 }
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   const query = '*[_type=="product"]';
   const products = await client.fetch(query);
   const bannerQuery = '*[_type=="banner"]';
