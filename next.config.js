a51985581aa309971a5abad26caec179522c3e4c
George <gex7777@gmail.com>
2022-12-06 12:19:48 +0530

    fix error

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@ const withPWA = require("next-pwa")({
   dest: "public",
 });
 /** @type {import('next').NextConfig} */
-const settings = {
+const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    // assuming you were using the Sanity.io image CDN
     // domains is an array of comma-separated strings
     // ['cdn.sanity.io', 'cdn.not-sanity.io', 'another domain']
     domains: ["cdn.sanity.io"],
@@ -16,6 +17,6 @@ const settings = {
     skipWaiting: true,
     disable: process.env.NODE_ENV === "development",
   },
-};
-module.exports =
-  process.env.NODE_ENV === "development" ? settings : withPWA(settings);
+});
+
+module.exports = nextConfig;
