ba138f7f911260ae0668022dfa572cdf29ee9f21
George <gex7777@gmail.com>
2022-12-06 12:14:14 +0530

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,13 +1,21 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const settings = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    // assuming you were using the Sanity.io image CDN
     // domains is an array of comma-separated strings
     // ['cdn.sanity.io', 'cdn.not-sanity.io', 'another domain']
     domains: ["cdn.sanity.io"],
   },
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
 };
-
-module.exports = nextConfig;
+module.exports =
+  process.env.NODE_ENV === "development" ? settings : withPWA(settings);
--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,14 @@ export default function Document() {
   return (
     <Html data-theme="cupcake">
       <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
         <meta
           name="description"
           content="Jancy teacher's food products company's very own online store! Taking orders for cakes, snacks and more!"
         ></meta>
+
+        <meta name="theme-color" content="#fff" />
       </Head>
 
       <Main />
